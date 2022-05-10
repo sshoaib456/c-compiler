b3ee0d8d05f4776a336474f4d4742ddd8433f633
Syed Shoaib UR Rehman <86778601+sshoaib456@users.noreply.github.com>
2022-05-10 18:36:09 +0500

    Add files via upload

---
+++ 123.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1, num2;
+    
+    cout << "Hello world!"<<endl;
+    
+    cout<<"Enter two number: ";
+    cin>>num1>>num2;
+    cout<<"The first number is: "<num1<<endl;
+    cout<<"The second number is: "<num2<<endl;
+    
+    return 0;
+}
